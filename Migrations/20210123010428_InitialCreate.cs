33783213b373886945ec0d972c7f1c26e12e6e2f
Matt Chapman <matt@mattchapman.io>
2021-01-23 14:25:48 +1300

    Add files' '

---
+++ Migrations/20210123010428_InitialCreate.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SilverSpy.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "payments",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    code = table.Column<string>(type: "text", nullable: true),
+                    frequency = table.Column<string>(type: "text", nullable: true),
+                    day = table.Column<int>(type: "integer", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_payments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    date_processed = table.Column<string>(type: "text", nullable: true),
+                    date = table.Column<string>(type: "text", nullable: true),
+                    type = table.Column<string>(type: "text", nullable: true),
+                    reference = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transactions", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "payments");
+
+            migrationBuilder.DropTable(
+                name: "transactions");
+        }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace SilverSpy.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string DateProcessed { get; set; }
+        public string Date { get; set; }
+        public string Type { get; set; }
+        public string Reference { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
