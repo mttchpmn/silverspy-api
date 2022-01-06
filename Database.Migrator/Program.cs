bd0eed5abfe875c094830c7cb1b478dd071172e8
Matt Chapman <matt@mattchapman.io>
2022-01-07 10:57:40 +1300

    0004 Add DB infx

--- Database.Migrator/Program.cs
+++ Database.Migrator/Program.cs
@@ -6,18 +6,24 @@ namespace Database
     {
         static int Main(string[] args)
         {
-            // TODO - Move connection string to environment variable
-            var connectionString = "Server=localhost;Port=5432;Database=silverspy;User ID=postgres;Password=postgres";
-
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            if (connectionString == null)
+            {
+                Console.Error.WriteLine("DB_CONNECTION_STRING env variable not set.");
+                return 1;
+            }
+            Console.WriteLine($"Connecting to DB: {connectionString}");
+            // var connectionString = "Server=localhost;Port=5432;Database=silverspy;User ID=postgres;Password=postgres";
+            
             var databaseMigrator = new DatabaseHelper(connectionString);
             var result = databaseMigrator.MigrateDatabase(connectionString);
-
+            
             if (!result.Successful)
             {
                 Console.Error.WriteLine("Error migrating database");
                 return -1;
             }
-
+            
             Console.WriteLine("Database migrated successfully.");
             return 0;
         }
