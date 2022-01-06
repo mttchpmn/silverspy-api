37639f9df04e9bbac440607b4f099392fe09c891
Matt Chapman <matt@mattchapman.io>
2022-01-07 11:25:30 +1300

    0001 Add TransactionsController

---
+++ Api/Controllers/TransactionsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TransactionsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Import()
+    {
+        return Ok("Imported successfully");
+    }
+}
