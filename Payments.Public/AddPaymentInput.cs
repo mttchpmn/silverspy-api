1a2cf43d39ef76061dd4e065be92c02916de4770
Matt Chapman <matt@mattchapman.io>
2022-01-25 19:00:42 +1300

    0006 Add input DTO

---
+++ Payments.Public/AddPaymentInput.cs
@@ -0,0 +1,11 @@
+﻿namespace Payments.Public;
+
+public record AddPaymentInput(
+    string ReferenceDate,
+    string Type,
+    string Frequency,
+    string Name,
+    string Category,
+    string Details,
+    decimal Amount
+);
