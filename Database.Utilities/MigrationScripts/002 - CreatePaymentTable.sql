bcd86f46e2608227f802ffc131eb1bf36f4cf16e
Matt Chapman <matt@mattchapman.io>
2022-01-25 19:21:24 +1300

    0006 Add migration script

---
+++ Database.Utilities/MigrationScripts/002 - CreatePaymentTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE payment
+(
+    id             SERIAL PRIMARY KEY,
+    auth_id        VARCHAR(255) NOT NULL,
+    transaction_id VARCHAR(255) NOT NULL,
+    reference_date timestamptz  NOT NULL,
+    type           integer      NOT NULL,
+    frequency      integer      NOT NULL,
+    value          money        NOT NULL,
+    name           VARCHAR(255),
+    category       VARCHAR(255),
+    details        VARCHAR(255)
+);
