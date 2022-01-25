c8c3463bc726c3d5295db77038cb353b05a62356
Matt Chapman <matt@mattchapman.io>
2022-01-25 18:22:57 +1300

    0006 Add dummy endpoints

---
+++ Api/Controllers/PaymentsController.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Transactions.Public;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public class PaymentsController : ControllerBase
+{
+
+    public PaymentsController()
+    {
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddPayment()
+    {
+        var authId = GetAuthId();
+        var result = "Add payment!";
+
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllPayments()
+    {
+        var authId = GetAuthId();
+        var result = "Get all payments";
+
+        return Ok(result);
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdatePayment()
+    {
+        var authId = GetAuthId();
+        var result = "Update payment!";
+
+        return Ok(result);
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> DeletePayment()
+    {
+        var authId = GetAuthId();
+        var result = "Delete payment!";
+
+        return Ok(result);
+    }
+    private string GetAuthId()
+    {
+        var authId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (authId == null)
+            throw new Exception("Auth ID is null");
+        
+        return authId;
+    }
+
+}
