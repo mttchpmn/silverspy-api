54123aa91e01d0a36775cd55dd0bc7f3076c5c45
Matt Chapman <matt@mattchapman.io>
2022-01-25 18:41:11 +1300

    0006 Add logic to generate payments

---
+++ Payments.Public/PaymentExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Payments.Public;
+
+public static class PaymentExtensions
+{
+   public static IEnumerable<DateTime> GenerateDates(this Payment payment, DateTime startDate, DateTime endDate)
+   {
+      var result = new List<DateTime>();
+      var referenceDate = payment.ReferenceDate;
+
+      while (referenceDate < endDate)
+      {
+         referenceDate = payment.Frequency switch
+         {
+            PaymentFrequency.Weekly => referenceDate.AddDays(7),
+            PaymentFrequency.Fortnightly => referenceDate.AddDays(14),
+            PaymentFrequency.Monthly => referenceDate.AddMonths(1),
+            PaymentFrequency.Yearly => referenceDate.AddYears(1),
+            _ => referenceDate
+         };
+         
+         if (referenceDate >= startDate)
+            result.Add(referenceDate);
+      }
+
+      return result;
+   }
+}
--- Payments.Public/PaymentFrequency.cs
+++ Payments.Public/PaymentFrequency.cs
@@ -2,7 +2,6 @@
 
 public enum PaymentFrequency
 {
-    OneOff,
     Weekly,
     Fortnightly,
     Monthly,
